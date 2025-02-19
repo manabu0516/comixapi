88d7a042230c3428f2c5d090bfc6e7dc8098436e
Fujimoto Manabu（藤本 学） <mfujimoto@bridge-g.com>
2025-02-19 09:47:57 +0900

    update.

--- index.js
+++ index.js
@@ -16,6 +16,35 @@ app.use(express.urlencoded({ extended: true}))
 
 app.listen(3000, console.log('Server listening port 3000'));
 
+const cache = (() => {
+    const crypto = require('crypto');
+
+    const hashKey = async (key) => {
+        return crypto.createHash('md5').update(key).digest('hex')
+    };
+
+    const put = async (key, content) => {
+        const fullPath = pathModule.join(processIofo.current, '_meta', 'cache', key + '.jpg');
+        fs.writeFile(fullPath, content);
+        return content;
+    };
+
+    const contains = async (key) => {
+        try {
+            const fullPath = pathModule.join(processIofo.current, '_meta', 'cache', key + '.jpg');
+            const stat = fs.stat(fullPath);
+            return await stat.isFile();
+        } catch(e) {
+            return false;
+        }
+    };
+
+    return {
+        hashKey : hashKey,
+        put : put,
+        contains : contains,
+    };
+})();
 
 app.get('/comics', async (req, res) => {
     try {
@@ -27,7 +56,7 @@ app.get('/comics', async (req, res) => {
             const fullPath = pathModule.join(processIofo.current, f);
 
             const stat = await fs.stat(fullPath);
-            if(stat.isDirectory() === true) {
+            if(stat.isDirectory() === true &&  f.startsWith("_") === false) {
                 result.push(f);
             }
         }
@@ -77,21 +106,28 @@ app.get('/volume/thumbnail/:id/:vol', async (req, res) => {
     try {
         const fullPath = pathModule.join(processIofo.current, req.params.id, req.params.vol);
 
+        const cacheKey = await cache.hashKey(fullPath + ':thumbnail');
+        if(cache.contains(cacheKey) === true) {
+            res.redirect('/cache/' + cacheKey);
+            return;
+        }
+        
         const data = await fs.readFile(fullPath);
         const zip = jszip();
         await zip.loadAsync(data);
-
         for (const fileName in zip.files) {
             const file = zip.files[fileName];
             if(file.dir === true) {
                 continue;
             }
-
+    
             const content = await file.async('nodebuffer');
-            res.type('jpg');
-            res.send(content);
+            cacheData = cache.put(cacheKey, content);
             break;
         }
+
+        res.type('jpg');
+        res.send(cacheData);
     } catch (e) {
         res.status(500).send('internal server error');
         console.log(e);
@@ -100,7 +136,6 @@ app.get('/volume/thumbnail/:id/:vol', async (req, res) => {
 
 app.get('/volume/entries/:id/:vol', async (req, res) => {
     
-
     try {
         const result = [];
         const fullPath = pathModule.join(processIofo.current, req.params.id, req.params.vol);
@@ -132,7 +167,13 @@ app.get('/page/:id/:vol', async (req, res) => {
     try {
         const fullPath = pathModule.join(processIofo.current, req.params.id, req.params.vol);
         const filename = req.query['p'];
-        
+
+        const cacheKey = await cache.hashKey(fullPath + ':' +filename);
+        if(cache.contains(cacheKey) === true) {
+            res.redirect('/cache/' + cacheKey);
+            return;
+        }
+
         const data = await fs.readFile(fullPath);
         const zip = jszip();
         await zip.loadAsync(data);
@@ -140,13 +181,15 @@ app.get('/page/:id/:vol', async (req, res) => {
         const file = zip.files[filename];
         
         const content = await file.async('nodebuffer');
-        res.type('jpg');
-        res.send(content);
+        cacheData = cache.put(cacheKey, content);
 
+        res.type('jpg');
+        res.send(cacheData);
     } catch (e) {
         res.status(500).send('internal server error');
         console.log(e);
     }
 });
 
-app.use('/static', express.static(pathModule.join(processIofo.module, 'public')))
+app.use('/static', express.static(pathModule.join(processIofo.module, 'public')));
+app.use('/static', express.static(pathModule.join(processIofo.current, '_meta', 'cache')));
